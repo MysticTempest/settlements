bbcfc0506f41f956eaa953281bc16d75eed872d1
MysticTempest <molecularmarc.he@gmail.com>
2019-02-10 18:47:12 -0600

    Increase spawn rate of butcher building.

--- const.lua
+++ const.lua
@@ -33,6 +33,8 @@ schem_path = settlements.modpath.."/schematics/"
 --
 -- list of schematics
 --
+--Notes: hwidth,hdepth,hheight are what considered when spawning houses. I copied the library code to the butcher & the butcher now spawns more often.
+--Previously it had a low spawn due to being bigger. But adjusted max_num as they spawned too close to each other initially.
 schematic_table = { 
   {name = "large_house", mts = schem_path.."large_house.mts", hwidth = 8, hdepth = 10, hheight = 11, hsize = 15, max_num = 0, rplc = "n"},
   {name = "well", mts = schem_path.."well.mts", hwidth = 6, hdepth = 8, hheight = 9, hsize = 11, max_num = 0.045, rplc = "n"},
@@ -40,7 +42,7 @@ schematic_table = {
   {name = "farm", mts = schem_path.."farm.mts", hwidth = 9, hdepth = 7, hheight = 8, hsize = 11, max_num = 0.1, rplc = "n"},
   {name = "lamp", mts = schem_path.."lamp.mts", hwidth = 3, hdepth = 4, hheight = 6, hsize = 10, max_num = 0.1, rplc = "n"},
   {name = "library", mts = schem_path.."library.mts", hwidth = 8, hdepth = 9, hheight = 10, hsize = 11, max_num = 0.055, rplc = "n"},
-  {name = "butcher", mts = schem_path.."butcher.mts", hwidth = 11, hdepth = 8, hheight = 10, hsize = 17, max_num = 0.050, rplc = "n"},
+  {name = "butcher", mts = schem_path.."butcher.mts", hwidth = 8, hdepth = 9, hheight = 10, hsize = 11, max_num = 0.065, rplc = "n"},
   {name = "blacksmith", mts = schem_path.."blacksmith.mts", hwidth = 7, hdepth = 11, hheight = 8, hsize = 11, max_num = 0.050, rplc = "n"},
 }
 --
